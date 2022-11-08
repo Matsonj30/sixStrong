1cc18a30f9137f7f656ac2c8919d45f97dc1eecf
Matsonj30 <matsonj2013@gmail.com>
2022-11-07 18:11:55 -0700

    ' '

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,11 @@
 function burgerMenu(){
     var checkbox = document.getElementById("burgerCheckbox");
     var menu = document.getElementById("menu");
-    console.log(checkbox.checked)
+    
     if(checkbox.checked == true){
-       //menu.style.width = "150px";
-  
         menu.style.transform = "none";
-    
-    
     }
     else{
-       // menu.style.width = "0px"
-     
         menu.style.transform = "translate(200px)";
-    
     }
 }
